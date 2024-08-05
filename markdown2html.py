7f58cd7f040cfb0a3ae67d329176f77a894b541c
Spacefox95 <nathan.raynal@gmail.com>
2024-08-05 15:04:25 +0200

    test

--- markdown2html.py
+++ markdown2html.py
@@ -6,17 +6,17 @@ import sys
 import os.path
 
 if len(sys.argv) != 3 :
-	print("Usage: ./markdown2html.py README.md README.html")
-	exit(1)
+	sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+	sys.exit(1)
 mdfile = sys.argv[1]
 htmlfile = sys.argv[2]
 
 if not os.path.isfile(mdfile):
-	print("Missing {}".format(mdfile))
-	exit(1)
+	sys.stderr.write("Missing {}\n".format(mdfile))
+	sys.exit(1)
 else:
 	with open(mdfile, 'r') as f:
 			content = f.read()
 			with open(htmlfile, 'w') as file:
 				file.write(content)
-	exit(0)
+	sys.exit(0)
