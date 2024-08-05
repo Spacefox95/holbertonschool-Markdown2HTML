e66ef7df6d1dac87dc9a191c85205531cbc78dde
Spacefox95 <nathan.raynal@gmail.com>
2024-08-05 14:09:34 +0200

    Python

---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Script to translate Markdow in HTML """
+
+import sys
+import os.path
+
+if len(sys.argv) != 3 :
+	print("Usage: ./markdown2html.py README.md README.html")
+	exit(1)
+mdfile = sys.argv[1]
+htmlfile = sys.argv[2]
+
+if not os.path.isfile(mdfile):
+	print("Missing {}".format(mdfile))
+	exit(1)
+else:
+	exit(0)
